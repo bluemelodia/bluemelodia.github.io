d9f5422bafcf06a37d41e9baab758b87b4e2ab27
Melanie Hsu <bluemelodia@users.noreply.github.com>
2015-04-20 16:15:31 -0400

    Update bluescript.js

--- bluescript.js
+++ bluescript.js
@@ -1,7 +1,55 @@
 
- //print the current time to the website
- var today = new Date();
- var str = String(d.getHours()) + " " + String(d.getMinutes()) + " " + String(d.getSeconds());
- alert(str);
- str = str.fontcolor("white");
- document.write(str);
+var slider = {
+  
+  el: {
+    slider: $("#slider"),
+    allSlides: $(".slide"),
+    sliderNav: $(".slider-nav"),
+    allNavButtons: $(".slider-nav > a")
+  },
+  
+  timing: 800,
+  slideWidth: 300, // could measure this
+  
+  init: function() {
+    // You can either manually scroll...
+    this.el.slider.on("scroll", function(event) {
+      slider.moveSlidePosition(event);
+    });
+    // ... or click a thing
+    this.el.sliderNav.on("click", "a", function(event) {
+      slider.handleNavClick(event, this);
+    });
+  },
+  
+  moveSlidePosition: function(event) {
+    // Magic Numbers
+    this.el.allSlides.css({
+      "background-position": $(event.target).scrollLeft()/6-100+ "px 0"
+    });  
+  },
+  
+  handleNavClick: function(event, el) {
+    // Don't change URL to a hash, remove if you want that
+    event.preventDefault();
+
+    // Get "1" from "#slide-1", for example
+    var position = $(el).attr("href").split("-").pop();
+    
+    this.el.slider.animate({
+      scrollLeft: position * this.slideWidth
+    }, this.timing);
+    
+    this.changeActiveNav(el);
+  },
+  
+  changeActiveNav: function(el) {
+    // Remove active from all links
+    this.el.allNavButtons.removeClass("active");
+    // Add back to the one that was pressed
+    $(el).addClass("active");
+  }
+  
+};
+
+slider.init();

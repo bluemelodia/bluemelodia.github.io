701640304969ee30805bbd47a03394d54173f1e4
Melanie Hsu <bluemelodia@users.noreply.github.com>
2015-04-20 12:53:33 -0400

    Update bluescript.js

--- bluescript.js
+++ bluescript.js
@@ -2,6 +2,6 @@
  //print the current time to the website
  var today = new Date();
  var str = String(d.getHours()) + " " + String(d.getMinutes()) + " " + String(d.getSeconds());
- alert(str)l
+ alert(str);
  str = str.fontcolor("white");
  document.write(str);

c493cd4606752ae6d97bc97d3d10f01bdc51bb41
Melanie Hsu <bluemelodia@users.noreply.github.com>
2015-06-07 22:00:01 -0400

    Update blue.js

--- blue.js
+++ blue.js
@@ -6,3 +6,20 @@ $(document).ready(function() {
 		$(this).removeClass('hover');
 	});
 });
+
+$(document).ready(function() {
+	var ww = document.body.clientWidth;
+	if (ww < 800) {
+		$(".toggleMenu").css("display", "inline-block");
+		$(".nav li a").click(function() {
+			$(this).parent("li").toggleClass('hover');
+		});
+	} else {
+		$(".toggleMenu").css("display", "none");
+		$(".nav li").hover(function() {
+			$(this).addClass('hover');
+		}, function() {
+			$(this).removeClass('hover');
+		});
+	}
+});
